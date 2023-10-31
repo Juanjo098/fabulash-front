e2562dcf47af54fd3769d265b2d1f062661e139e
Juanjo098 <juanjo.silva.098@gmail.com>
2023-10-31 09:10:53 -0600

    fix(home): :art: change inicio.css
    change in inicio.css
    
    1.0.1

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import { lazy, Suspense } from 'react'
 import { Routes, Route } from 'react-router-dom'
 
-import './App.css'
-
 const HomePage = lazy(() => import('./pages/HomePage.jsx'))
 const LoginPage = lazy(() => import('./pages/LoginPage.jsx'))
 const RegisterPage = lazy(() => import('./pages/RegisterPage.jsx'))
